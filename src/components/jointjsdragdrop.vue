79a3c62da2c1795461983587a3a15c97f1dcae52
siddhesh <siddhesh@erpdata.cloud>
2018-08-07 15:50:45 +0530

    demo

--- src/components/jointjsdragdrop.vue
+++ src/components/jointjsdragdrop.vue
@@ -12,7 +12,9 @@
 			var graph = new joint.dia.Graph,
 			  paper = new joint.dia.Paper({
 			    el: $('#paper'),
-			    model: graph
+			    model: graph,
+			    gridSize: 5,
+  				drawGrid:true
 			  });
 			// Canvas from which you take shapes
 			var stencilGraph = new joint.dia.Graph,
@@ -22,35 +24,40 @@
 			    model: stencilGraph,
 			    interactive: false
 			  });
-		var link = new joint.shapes.standard.Link();
-		// 	graph.options.defaultAnchor = {
-		//     name: 'midSide',
-		//     args: {
-		//         rotate: true,
-		//         padding: 20
-		//     }
-		// };
-		var selected;
 
-		paper.on('cell:pointerdblclick', function(cellView, evt, x, y) {
-		    cellView.remove();
-		});
+		paper.on('cell:keydown', function(cellView,e) {
+				debugger;
+        		alert(e.which)
+    		});
 
-			var r1 = new joint.shapes.basic.Rect({
-			  position: {
-			    x: 10,
-			    y: 10
-			  },
-			  size: {
-			    width: 100,
-			    height: 40
-			  },
-			  attrs: {
-			    text: {
-			      text: 'Rect1'
-			    },
-			  },
-			});
+		paper.on('element:pointerdblclick', function(cellView, evt, x, y) {
+		    //cellView.remove();
+		    debugger;
+		    var shapeText = prompt('Enter your table name:', '');
+		    cellView.model.attr('text/text', shapeText)
+		    cellView.model.attr('rect/title', 'abc')
+		    // console.log(graph)
+
+		}).on('cell:contextmenu', function(cellView,e) {
+				debugger;
+        		alert(cellView.model.id)
+    		});
+
+			// var r1 = new joint.shapes.basic.Rect({
+			//   position: {
+			//     x: 10,
+			//     y: 10
+			//   },
+			//   size: {
+			//     width: 100,
+			//     height: 40
+			//   },
+			//   attrs: {
+			//     text: {
+			//       text: 'Rect1'
+			//     },
+			//   },
+			// });
 			var model = new joint.shapes.devs.Model({
 			  position: {
 			    x: 230,
@@ -96,10 +103,105 @@
 			  attrs: {
 			    text: {
 			      text: 'Rect2'
-			    }
+			    },
+			    image : {
+		            "xlink:href" : "../../static/flowchart/images/archive.png",
+		            width : 200,
+		            height : 100
+		        	}
 			  }
 			});
-			stencilGraph.addCells([r1, r2,model]);
+
+			joint.shapes.devs.MyImageModel = joint.shapes.devs.Model.extend({
+
+			    markup: '<g class="rotatable"><g class="scalable"><rect class="body"/></g><image/><text class="label"/><g class="inPorts"/><g class="outPorts"/></g>',
+
+			    defaults: joint.util.deepSupplement({
+
+			        type: 'devs.MyImageModel',
+			        size: { width: 100, height: 40 },
+			        attrs: {
+			            '.port-body': {
+			                r: 5,
+			                magnet: true,
+			                stroke: '#000000'
+			            },
+			            '.inPorts circle': { fill: '' },
+			            '.outPorts circle': { fill: '' },
+			            image: { 'xlink:href': '../../static/flowchart/images/archive.png', width: 100, height: 40, 'ref-x': .5, 'ref-y': .5, ref: 'rect', 'x-alignment': 'middle', 'y-alignment': 'middle'}
+			        }
+
+			    }, joint.shapes.devs.Model.prototype.defaults)
+			});
+
+			joint.shapes.devs.MyImageModelView = joint.shapes.devs.ModelView;
+
+
+			var imageModel = new joint.shapes.devs.MyImageModel({
+			    position: { x: 10, y: 10 },
+			    size: { width: 100, height: 40 },
+			    attrs: {
+			    text: {
+			      text: ''
+			    }
+				},
+			  inPorts: ['a'],
+			  outPorts: ['b'],
+			  ports: {
+			    groups: {
+			      'in': {
+			        position: 'top',
+			        label: {
+			          position: 'outside'
+			        }
+			      },
+			      'out': {
+			        position: 'bottom',
+			        label: {
+			          position: 'outside'
+			        }
+			      }
+			    }
+			  },
+			});
+
+			var image = new joint.shapes.basic.Image({
+		    position: {
+			    x: 10,
+			    y: 10
+			  },
+			  size: {
+			    width: 100,
+			    height: 40
+			  },
+			  inPorts: ['a'],
+			  outPorts: ['b'],
+			  ports: {
+			    groups: {
+			      'in': {
+			        position: 'top',
+			        label: {
+			          position: 'outside'
+			        }
+			      },
+			      'out': {
+			        position: 'bottom',
+			        label: {
+			          position: 'outside'
+			        }
+			      }
+			    }
+			  },
+		    attrs : {
+		        image : {
+		            "xlink:href" : "../../static/flowchart/images/archive.png",
+		            width : 200,
+		            height : 100
+		        	}
+		    	}
+			});
+
+			stencilGraph.addCells([imageModel, r2,model]);
 
 			stencilPaper.on('cell:pointerdown', function(cellView, e, x, y) {
 			  $('body').append('<div id="flyPaper" style="position:fixed;z-index:100;opacity:.7;pointer-event:none;"></div>');
